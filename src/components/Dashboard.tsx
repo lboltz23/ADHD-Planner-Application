cfc036cdd8232e4ddf0770ef53117f5272ceb7ab
Zayne <zayne5504@hotmail.com>
2026-02-03 18:25:34 -0600

    Update Dashboard.tsx
    Added Import for TaskType Selector as well as removed dupe import

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -9,10 +9,10 @@ import {
   View
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Settings, Calendar, Zap } from 'lucide-react-native';
 import { Task, TaskType, CreateTaskParams } from '../types';
 import { SettingsData } from './Settings';
 import { TaskCard } from './TaskCard';
+import { TaskTypeSelector } from './TaskTypeSelector';
 import AddTaskDialog from './AddTaskDialog';
 
 // Dashboard Props
