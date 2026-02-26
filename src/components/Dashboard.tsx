cf429d7d3a8c6d4df34b0ed3df1b014ed1fc694f
adiotope12 <149920417+adiotope12@users.noreply.github.com>
2026-02-26 13:59:39 -0600

    fixed length on completedTodayTasks

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -156,7 +156,7 @@ export function Dashboard({
   };
 
   const handleProgressBar = () => {
-    if (completedTodayTasks.length === 0)
+    if (completedTodayTasks === 0)
       return false;
     return true;
   }
